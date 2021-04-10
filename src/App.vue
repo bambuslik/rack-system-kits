98e71846b205a9c9e31cb813acf669333b39de71
Kostya Zhi <smart.index@gmail.com>
2021-04-10 17:55:40 +0300

    add pathway component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
-<template>
-  <p>init</p>
+<template lang="pug">
+.products-list-page
+    header.products-list-page__header.page-wrapper
+        app-pathway(:pathway="pathway")
+    aside.products-list-page__filters.page-wrapper
+    main.products-list-page__items.page-wrapper
+
+
+
+
 </template>
 
 <script>
+    //import components
+    import AppPathway from '@/components/AppPathway/index.vue'
+    import AppPageTitle from '@/components/AppPageTitle/index.vue'
 
 export default {
-  name: 'App',
-  components: {
+    name: 'App',
+    components: {
+        AppPathway
+    },
+    setup() {
+        //получаем pathway из роутера
+        const pathway = [
+            {
+                title: 'Главная',
+                link: '#'
+            },
+            {
+                title: 'Системы хранения',
+                link: '#'
+            },
+            {
+                title: 'Комплекты стеллажных систем',
+                link: ''
+            }
+        ]
 
-  }
+        return {
+            pathway
+
+        }
+    }
+
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    .products-list-page {
+        &__wrapper {
+
+        }
+    }
+
 
 </style>
---
+++ src/components/AppPageTitle/index.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+ul.pathway(v-if="pathway.length")
+    li.pathway__item(v-for="(pathwayItem, key) in pathway" :key="key") {{ pathwayItem.title }}
+</template>
+
+<script>
+export default {
+    name: 'AppPathway',
+    components: {
+
+    },
+    props: ['pathway'],
+    setup() {
+
+        return {
+
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .pathway {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-wrap: wrap;
+        font-family: 'SF UI Text', Arial, sans-serif;
+        line-height: 1;
+
+        &__item {
+            color: #828282;
+
+            &:after {
+                content: '/';
+                display: inline-block;
+                margin: 0 14px;
+            }
+
+            &:last-child {
+                color: $color-main;
+
+                &:after {
+                    display: none;
+                }
+            }
+        }
+    }
+</style>
