a5c18e5f4da66ce7767b0f7efff849830fc0f342
Kostya Zhi <smart.index@gmail.com>
2021-04-11 22:37:10 +0300

    add product buttons, sale

--- src/components/Products/ProductsItem.vue
+++ src/components/Products/ProductsItem.vue
@@ -6,6 +6,12 @@
     .product__row.product__row_price
         span.product__price.product__price_old(v-if="product.price.old_price") {{ Math.round(product.price.old_price) }}₽
         span.product__price.product__price_current {{ Math.round(product.price.current_price) }}₽
+    .product__buttons
+        button.product__btn.product__btn_cart()
+        //button.product__btn.product__btn_cart.product__btn_cart-active()
+        button.product__btn.product__btn_like()
+        //button.product__btn.product__btn_like.product__btn_like-active()
+    span.product__sale(v-if="product.price.old_price") Скидка
 </template>
 
 <script>
@@ -31,6 +37,7 @@ export default {
 
 <style lang="scss" scoped>
     .product {
+        position: relative;
         padding: 9px;
         border: 1px solid #EEEEEE;
         flex-grow: 1;
@@ -78,9 +85,62 @@ export default {
                 color: #888888;
                 text-decoration: line-through;
             }
+        }
 
+        &__buttons {
+            position: absolute;
+            bottom: 4px;
+            right: 14px;
+        }
+
+        &__btn {
+            padding: 0;
+            width: 30px;
+            height: 30px;
+            background: transparent;
+            border: none;
+            cursor: pointer;
+            outline: none;
+            transition: opacity .3s ease;
+
+            &:hover {
+                opacity: .6;
+            }
+
+            &_cart {
+                background: url('~@/assets/img/product/cart-ico.svg') no-repeat center;
+
+                &-active {
+                    background: url('~@/assets/img/product/cart-active-ico.svg') no-repeat center;
+                }
+            }
+
+            &_like {
+                margin-left: 16px;
+                background: url('~@/assets/img/product/heart-ico.svg') no-repeat center;
+
+                &-active {
+                    background: url('~@/assets/img/product/heart-active-ico.svg') no-repeat center;
+                }
+            }
+        }
+
+        &__sale {
+            position: absolute;
+            top: 8px;
+            left: 0;
+            background: #EB5757;
+            font-family: 'SF Pro Display';
+            color: #FFF;
+            font-size: 14px;
+            line-height: 1.3;
+            font-weight: 500;
+            letter-spacing: 1px;
+            padding: 3px 16px;
 
         }
+
+
 
     }
 
